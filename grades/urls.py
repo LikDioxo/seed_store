34ef5073c0d587da6df818e9d7a7d593b7c3dca1
olharums <olha.rumiantseva13@gmail.com>
2020-10-15 18:19:06 +0300

    Olechka

--- grades/urls.py
+++ grades/urls.py
@@ -7,5 +7,4 @@ app_name = 'grades'
 urlpatterns = [
     path('', views.get_all_grades_by_category_and_kind_id),
     path('<int:grade_id>/', views.get_grade_by_id),
-    path('<int:year>/', views.get_by_year)
 ]
